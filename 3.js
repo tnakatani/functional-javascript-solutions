09092d023c4a12e9e42d4013854223313bb0622c
tnakatani <tnakatani@gmail.com>
2015-02-24 15:31:04 +0900

    init commit

---
+++ 3.js
@@ -0,0 +1,20 @@
+// 3 - Basic: Map
+
+function doubleAll (numbers) {
+    var result = [];
+    numbers.map(function (num) {
+        result.push(num * 2);
+    });
+    return result;
+}
+
+// Official solution
+// function doubleAll(numbers) {
+//         return numbers.map(function double(num) {
+//         return num * 2;
+//     });
+// }
+
+// Note: still don't understand returning functions...
+
+module.exports = doubleAll;
